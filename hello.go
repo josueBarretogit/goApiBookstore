87e1e286aabacd36004a9422fc984af8e3a54942
josueBarretogit <josuebarretogit@gmail.com>
2023-12-17 21:08:49 -0500

    aplicacion for en go

--- hello.go
+++ hello.go
@@ -2,33 +2,22 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"time"
+	"math"
 )
 
-var javascript = "bad"
-var integer int = 24
-
-const (
-	// Create a huge number by shifting a 1 bit left 100 places.
-	// In other words, the binary number that is 1 followed by 100 zeroes.
-	Big = 1 << 100
-	// Shift it right again 99 places, so we end up with 1<<1, or 2.
-	Small = Big >> 99
-)
+func Sqrt(x float64) float64 {
+	for z := 1.0; z < 1000000; z += 1 {
+		if math.Pow(z, z) >= x {
+			fmt.Println(math.Pow(z, z))
+			fmt.Println("Found square root")
+			fmt.Println(z)
+			fmt.Println(z - 1)
+			return z - 1
+		}
+	}
+	return 0
+}
 
 func main() {
-	Goo := "good"
-	floating := float64(integer)
-	fmt.Println("Hello, World!", time.Now())
-	newString1, newString2 := swap("string 1", "string2")
-	val1, mandatoryVal2 := split(10)
-	fmt.Println(newString1, newString2)
-	fmt.Println(val1, mandatoryVal2)
-	fmt.Println(javascript)
-	fmt.Println(Goo)
-	fmt.Println(floating)
-	setEnv()
-	fmt.Print(os.Getenv("FOO"))
-	fmt.Print(os.Getenv("maxint"))
+	fmt.Println(Sqrt(4))
 }
